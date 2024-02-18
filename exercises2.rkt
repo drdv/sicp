328a01c2f2387fa7d37a0b76ed70350b6ed22b71
dimitrod <dimitar.dimitrov@kelkoogroup.com>
2024-02-18 09:16:13 +0100

    2.2

--- exercises2.rkt
+++ exercises2.rkt
@@ -3,11 +3,10 @@
 ;; =====================================================================================
 #lang racket/base
 
-;; (#%require (only (submod "exercises1.rkt" Exercise/1.20) gcd))
-
 (module Exercise/2.1 sicp
   (#%require (only racket/base module+ format let-values exn:fail?))
 
+  ;; there are simpler solutions but I wanted to learn how to use let-values
   (define (make-rat n d)
     (cond [(= d 0) (error "Not a number")]
           [else (let-values ([(n-normalized d-normalized)
@@ -74,13 +73,38 @@
      exn:fail?
      (lambda () (make-rat 1 0)))))
 
-;; (module Exercise/2.2 sicp
-;;   (#%require (only racket/base module+))
-;;   (module+ test
-;;     (#%require rackunit)
-;;     (display "==================== Exercise/2.2 ====================\n")
+(module Exercise/2.2 sicp
+  (#%require (only racket/base module+ format)
+             (only (submod "exercises1.rkt" common-utils) average tolerance))
 
-;;     ))
+  (define (make-segment point-a point-b) (cons point-a point-b))
+  (define (start-segment segment) (car segment))
+  (define (end-segment segment) (cdr segment))
+
+  (define (make-point x y) (cons x y))
+  (define (x-point point) (car point))
+  (define (y-point point) (cdr point))
+
+  (define (midpoint-segment segment)
+    (let ([point-a (start-segment segment)]
+          [point-b (end-segment segment)])
+      (make-point
+       (average (x-point point-a) (x-point point-b))
+       (average (y-point point-a) (y-point point-b)))))
+
+  (define (print-point point)
+    (display (format "(~a, ~a)" (x-point point) (y-point point))))
+
+  (module+ test
+    (#%require rackunit)
+    (display "==================== Exercise/2.2 ====================\n")
+
+    (let ([point (midpoint-segment (make-segment (make-point 1 2)
+                                                 (make-point 3 4)))])
+      (check-equal? (x-point point) 2.0)
+      (check-equal? (y-point point) 3.0)
+      (print-point point))))
 
 (module+ test
-  (require (submod ".." Exercise/2.1 test)))
+  (require (submod ".." Exercise/2.1 test))
+  (require (submod ".." Exercise/2.2 test)))
