b05f6ecd364c4b474a6ef4eae979b9fbeb19a9c6
dimitrod <dimitar.dimitrov@kelkoogroup.com>
2024-03-16 21:37:18 +0100

    2.56

--- exercises2.rkt
+++ exercises2.rkt
@@ -3140,6 +3140,129 @@ This module includes the push example from Lecture 3A. I found both the lecture 
       (check-equal? s1 s2)
       (check-equal? (car s1) (car s2)))))
 
+(module Example/symbolic-differentiation sicp
+  (#%provide variable?
+             same-variable?
+             sum?
+             addend
+             augend
+             product?
+             multiplier
+             multiplicand
+             =number?
+             make-sum
+             make-product)
+  (#%require (only racket/base module+))
+
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  (define (sum? x) (and (pair? x) (eq? (car x) '+)))
+  (define (addend s) (cadr s))
+  (define (augend s) (caddr s))
+  (define (product? x) (and (pair? x) (eq? (car x) '*)))
+  (define (multiplier p) (cadr p))
+  (define (multiplicand p) (caddr p))
+
+  (define (=number? exp num) (and (number? exp) (= exp num)))
+  (define (make-sum a1 a2)
+    (cond [(=number? a1 0) a2]
+          [(=number? a2 0) a1]
+          [(and (number? a1) (number? a2)) (+ a1 a2)]
+          [else (list '+ a1 a2)]))
+
+  (define (make-product m1 m2)
+    (cond [(or (=number? m1 0) (=number? m2 0)) 0]
+          [(=number? m1 1) m2]
+          [(=number? m2 1) m1]
+          [(and (number? m1) (number? m2)) (* m1 m2)]
+          [else (list '* m1 m2)]))
+
+  (define (deriv expr var)
+    (cond [(number? expr) 0]
+          [(variable? expr) (if (same-variable? expr var) 1 0)]
+          [(sum? expr) (make-sum (deriv (addend expr) var)
+                                 (deriv (augend expr) var))]
+          [(product? expr)
+           (make-sum
+            (make-product (multiplier expr)
+                          (deriv (multiplicand expr) var))
+            (make-product (deriv (multiplier expr) var)
+                          (multiplicand expr)))]
+          [else
+           (error "unknown expression type: DERIV" expr)]))
+
+  (module+ test
+    (#%require rackunit)
+    (display "--> Example/symbolic-differentiation\n")
+
+    (check-equal? (deriv 'x 'x) 1)
+    (check-equal? (deriv 'y 'x) 0)
+    (check-equal? (deriv '(+ x 3) 'x) 1)
+    (check-equal? (deriv '(* x y) 'x) 'y)
+    (check-equal? (deriv '(* (* x y) (+ x 3)) 'x) '(+ (* x y) (* y (+ x 3))))))
+
+(module Exercise/2.56 sicp
+  (#%provide exponentiation?
+             base
+             exponent
+             make-exponentiation
+             deriv)
+  (#%require (only racket/base module+)
+             (only (submod ".." Example/symbolic-differentiation)
+                   variable?
+                   same-variable?
+                   sum?
+                   addend
+                   augend
+                   product?
+                   multiplier
+                   multiplicand
+                   =number?
+                   make-sum
+                   make-product))
+
+  (define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
+  (define (base x) (cadr x))
+  (define (exponent x) (caddr x))
+  (define (make-exponentiation b n)
+    (cond [(=number? b 0) 0]
+          [(=number? n 0) 1]
+          [(=number? n 1) b]
+          [(and (number? b) (number? n)) (expt b n)]
+          [else (list '** b n)]))
+
+  (define (deriv expr var)
+    (cond [(number? expr) 0]
+          [(variable? expr) (if (same-variable? expr var) 1 0)]
+          [(sum? expr) (make-sum (deriv (addend expr) var)
+                                 (deriv (augend expr) var))]
+          [(product? expr)
+           (make-sum
+            (make-product (multiplier expr)
+                          (deriv (multiplicand expr) var))
+            (make-product (deriv (multiplier expr) var)
+                          (multiplicand expr)))]
+          [(exponentiation? expr)
+           (let ([b (base expr)]
+                 [n (exponent expr)])
+             (make-product
+              (make-product n (make-exponentiation b (make-sum n -1)))
+              (deriv b var)))]
+          [else
+           (error "unknown expression type: DERIV" expr)]))
+
+  (module+ test
+    (#%require rackunit)
+    (display "--> Exercise/2.56\n")
+
+    (let ([expr '(** (+ x 1) (+ y 2))])
+      (check-true (exponentiation? expr))
+      (check-equal? (base expr) '(+ x 1))
+      (check-equal? (exponent expr) '(+ y 2)))
+
+    ;; the simplified expression is 32*x + 24
+    (check-equal? (deriv '(** (+ (* 4 x) 3) 2) 'x) '(* (* 2 (+ (* 4 x) 3)) 4))))
+
 (module+ test
   (require (submod ".." Exercise/2.1 test)
            (submod ".." Exercise/2.2 test)
@@ -3205,4 +3328,6 @@ This module includes the push example from Lecture 3A. I found both the lecture 
            (submod ".." Lecture/3A test)
            (submod ".." Exercise/2.53 test)
            (submod ".." Exercise/2.54 test)
-           (submod ".." Exercise/2.55 test)))
+           (submod ".." Exercise/2.55 test)
+           (submod ".." Example/symbolic-differentiation test)
+           (submod ".." Exercise/2.56 test)))
